1a056e404f1649b590ebf07170c455cccbd497f6
hdbugsbunny <harshitpal26697@gmail.com>
2024-07-15 18:21:54 +0530

    Sending a POST HTTP Request, Handling Side Effects with useEffect()

--- react-crash-course/src/components/PostList.jsx
+++ react-crash-course/src/components/PostList.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Modal from "./Modal";
 import NewPost from "./NewPost";
 import Post from "./Post";
@@ -8,7 +8,24 @@ export default function PostList(props) {
   const { isPost, onStopPost } = props;
   const [posts, setPosts] = useState([]);
 
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await fetch("http://localhost:8080/posts");
+      const data = await response.json();
+      setPosts(data.posts);
+    }
+
+    fetchPosts();
+  }, []);
+
   function addPostHandler(postData) {
+    fetch("http://localhost:8080/posts", {
+      method: "POST",
+      body: JSON.stringify(postData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
     setPosts((prevPosts) => [postData, ...prevPosts]);
     onStopPost();
   }
@@ -22,8 +39,8 @@ export default function PostList(props) {
       )}
       {posts.length > 0 && (
         <ul className={classes.posts}>
-          {posts.map((post, index) => (
-            <Post key={index} author={post.author} body={post.body} />
+          {posts.map((post) => (
+            <Post key={post.id} author={post.author} body={post.body} />
           ))}
         </ul>
       )}
