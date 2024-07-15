a53191bd62828026affacd1a2cef1d495bb004b4
hdbugsbunny <harshitpal26697@gmail.com>
2024-07-15 13:56:17 +0530

    Adding Event Listeners

--- react-crash-course/src/components/NewPost.jsx
+++ react-crash-course/src/components/NewPost.jsx
@@ -1,11 +1,15 @@
 import classes from "./NewPost.module.css";
 
 export default function NewPost() {
+  function changeBodyHandler(event) {
+    console.log("🚀 ~ NewPost ~ event:", event.target.value);
+  }
+
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} />
+        <textarea id="body" required rows={3} onChange={changeBodyHandler} />
       </p>
       <p>
         <label htmlFor="name">Your Name</label>
--- react-crash-course/src/components/PostList.jsx
+++ react-crash-course/src/components/PostList.jsx
@@ -1,11 +1,15 @@
+import NewPost from "./NewPost";
 import Post from "./Post";
 import classes from "./PostList.module.css";
 
 export default function PostList() {
   return (
-    <ul className={classes.posts}>
-      <Post author="Harshit" body="React.js is Awesome!" />
-      <Post author="Manuel" body="Check Out The Full Course!" />
-    </ul>
+    <>
+      <NewPost />
+      <ul className={classes.posts}>
+        <Post author="Harshit" body="React.js is Awesome!" />
+        <Post author="Manuel" body="Check Out The Full Course!" />
+      </ul>
+    </>
   );
 }
