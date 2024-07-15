e511f8667ec5240d44192b1039306db24adc9683
hdbugsbunny <harshitpal26697@gmail.com>
2024-07-15 16:38:15 +0530

    Handling Form Submission

--- react-crash-course/src/components/PostList.jsx
+++ react-crash-course/src/components/PostList.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import Modal from "./Modal";
 import NewPost from "./NewPost";
 import Post from "./Post";
@@ -6,29 +5,16 @@ import classes from "./PostList.module.css";
 
 export default function PostList(props) {
   const { isPost, onStopPost } = props;
-  const [enteredBody, setEnteredBody] = useState("");
-  const [enteredAuthor, setEnteredAuthor] = useState("");
-
-  function bodyChangeHandler(event) {
-    setEnteredBody(event.target.value);
-  }
-
-  function authorChangeHandler(event) {
-    setEnteredAuthor(event.target.value);
-  }
 
   return (
     <>
       {isPost && (
         <Modal onClose={onStopPost}>
-          <NewPost
-            onBodyChange={bodyChangeHandler}
-            onAuthorChange={authorChangeHandler}
-          />
+          <NewPost onCancel={onStopPost} />
         </Modal>
       )}
       <ul className={classes.posts}>
-        <Post author={enteredAuthor} body={enteredBody} />
+        <Post author="Harshit" body="React.js is Awesome!" />
         <Post author="Manuel" body="Check Out The Full Course!" />
       </ul>
     </>
