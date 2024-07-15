c03a8d0ed097872c5f95535de15ac51ca4485b34
hdbugsbunny <harshitpal26697@gmail.com>
2024-07-15 13:20:49 +0530

    CSS Styling & CSS Modules

--- react-crash-course/src/components/Post.jsx
+++ react-crash-course/src/components/Post.jsx
@@ -1,9 +1,11 @@
+import classes from "./Post.module.css";
+
 export default function Post(props) {
   const { author, body } = props;
   return (
-    <div>
-      <p>{author}</p>
-      <p>{body}</p>
+    <div className={classes.post}>
+      <p className={classes.author}>{author}</p>
+      <p className={classes.text}>{body}</p>
     </div>
   );
 }
