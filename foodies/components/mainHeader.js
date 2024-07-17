8728531eeca90f2d284ae071b3a647dcbd007e6e
hdbugsbunny <harshitpal26697@gmail.com>
2024-07-17 11:55:32 +0530

    Styling Header Component Using CSS Modules

--- foodies/components/mainHeader.js
+++ foodies/components/mainHeader.js
@@ -1,14 +1,15 @@
 import logoImg from "@/assets/logo.png";
 import Link from "next/link";
+import classes from "./mainHeader.module.css";
 
 export default function MainHeader() {
   return (
-    <header>
-      <Link href={"/"}>
+    <header className={classes.header}>
+      <Link className={classes.logo} href={"/"}>
         <img src={logoImg.src} alt="A plate with food on it" />
         NextLevel Food
       </Link>
-      <nav>
+      <nav className={classes.nav}>
         <ul>
           <li>
             <Link href={"meals"}>Browse Meals</Link>
