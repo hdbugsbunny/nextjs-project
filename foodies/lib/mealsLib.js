689980901fb4552769ed50d43fbe896594af3e65
hdbugsbunny <harshitpal26697@gmail.com>
2024-09-18 18:19:06 +0530

    Handling Errors

--- foodies/lib/mealsLib.js
+++ foodies/lib/mealsLib.js
@@ -4,5 +4,6 @@ const mealsDB = sql("meals.db");
 
 export async function getMeals() {
   await new Promise((resolve, reject) => setTimeout(resolve, 2000));
+  // throw new Error("Fetching Meals Failed!");
   return mealsDB.prepare("SELECT * FROM meals").all();
 }
