4bc7bb709c324573b0076311843c198b66a3afff
hdbugsbunny <harshitpal26697@gmail.com>
2024-09-18 18:24:23 +0530

    Handling "Not Found" States

---
+++ foodies/app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Not Found</h1>
+      <p>Sorry, We Could Not Find The Requested Page/Resource!</p>
+    </main>
+  );
+}
