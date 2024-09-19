a54d949eb9cfdee2c7329604743dd4ed7e973c6b
hdbugsbunny <harshitpal26697@gmail.com>
2024-09-19 19:31:09 +0530

    Adding an Image Preview to the Picker

--- foodies/components/mealsGrid/image-picker.js
+++ foodies/components/mealsGrid/image-picker.js
@@ -1,15 +1,37 @@
 "use client";
 
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import classes from "./image-picker.module.css";
+import Image from "next/image";
 
 export default function ImagePicker({ label, name }) {
+  const [selectedImage, setSelectedImage] = useState(null);
   const inputRef = useRef();
+
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    if (!file) {
+      setSelectedImage(null);
+      return;
+    }
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      setSelectedImage(reader.result);
+    };
+    reader.readAsDataURL(file);
+  };
 
   return (
     <div className={classes.picker}>
       <label htmlFor={name}>{label}</label>
       <div className={classes.controls}>
+        <div className={classes.preview}>
+          {!selectedImage && <p>No Image is Selected Yet!</p>}
+          {selectedImage && (
+            <Image src={selectedImage} alt="User Selected Image" fill />
+          )}
+        </div>
         <input
           className={classes.input}
           type="file"
@@ -17,6 +39,7 @@ export default function ImagePicker({ label, name }) {
           accept="image/*"
           name={name}
           ref={inputRef}
+          onChange={handleImageChange}
         />
         <button
           className={classes.button}
