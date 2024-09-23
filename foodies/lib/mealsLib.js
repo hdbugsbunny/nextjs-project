59205b71c99e1e1daef50fd18da20f25c48f21e1
hdbugsbunny <harshitpal26697@gmail.com>
2024-09-23 17:27:31 +0530

    Storing Uploaded Images & Storing Data in the Database

--- foodies/lib/mealsLib.js
+++ foodies/lib/mealsLib.js
@@ -1,6 +1,7 @@
 import sql from "better-sqlite3";
 import slugify from "slugify";
 import xss from "xss";
+import fs from "node:fs";
 
 const mealsDB = sql("meals.db");
 
@@ -14,7 +15,39 @@ export function getMealsById(id) {
   return mealsDB.prepare("SELECT * FROM meals WHERE mealId = ?").get(id);
 }
 
-export function saveMeal(meal) {
+export async function saveMeal(meal) {
   meal.mealId = slugify(meal.title, { lower: true });
   meal.instructions = xss(meal.instructions);
+
+  const extension = meal.image.name.split(".").pop();
+  const fileName = `${meal.mealId}.${extension}`;
+
+  const stream = fs.createWriteStream(`public/images/${fileName}`);
+  const bufferedImage = await meal.image.arrayBuffer();
+
+  stream.write(Buffer.from(bufferedImage), (error) => {
+    if (error) {
+      throw new Error("Saving File Failed!");
+    }
+  });
+
+  meal.image = `/images/${fileName}`;
+
+  mealsDB
+    .prepare(
+      `
+    INSERT INTO meals 
+      (mealId, title, image, summary, instructions, creator, creator_email) 
+    VALUES (
+      @mealId,
+      @title,
+      @image,
+      @summary,
+      @instructions,
+      @creator,
+      @creator_email
+    )
+ `
+    )
+    .run(meal);
 }
