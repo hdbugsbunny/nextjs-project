b6b38e528fb7f698531e2ce376f9e0abb0b8174a
hdbugsbunny <harshitpal26697@gmail.com>
2024-10-01 17:47:28 +0530

    Catch-All Fallback Routes & Dealing With Multiple Path Segments

--- news-blog/app/archive/@archive/[[...filter]]/page.js
+++ news-blog/app/archive/@archive/[[...filter]]/page.js
@@ -1,24 +1,54 @@
-import { getAvailableNewsYears } from "@/lib/news";
+import NewsList from "@/components/newsList";
+import {
+  getAvailableNewsMonths,
+  getAvailableNewsYears,
+  getNewsForYear,
+  getNewsForYearAndMonth,
+} from "@/lib/news";
 import Link from "next/link";
 
 export default function FilteredNewsPage({ params }) {
   const { filter } = params;
-  const links = getAvailableNewsYears();
-  // const news = getNewsForYear(year);
+  const selectedYear = filter?.[0],
+    selectedMonth = filter?.[1];
+  let news,
+    links = getAvailableNewsYears(),
+    newsContent = <p>No News Found For The Selected Period!</p>;
+
+  if (selectedYear && !selectedMonth) {
+    news = getNewsForYear(selectedYear);
+    links = getAvailableNewsMonths(selectedYear);
+  }
+
+  if (selectedYear && selectedMonth) {
+    news = getNewsForYearAndMonth(selectedYear, selectedMonth);
+    links = [];
+  }
+
+  if (news && news.length > 0) {
+    newsContent = <NewsList news={news} />;
+  }
 
   return (
-    <header id="archive-header">
-      <nav>
-        <ul>
-          {links.map((link) => (
-            <li key={link}>
-              <Link href={`/archive/${link}`}>{link}</Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
-    </header>
-  );
+    <>
+      <header id="archive-header">
+        <nav>
+          <ul>
+            {links.map((link) => {
+              const href = selectedYear
+                ? `/archive/${selectedYear}/${link}`
+                : `/archive/${link}`;
 
-  // return <NewsList news={news} />;
+              return (
+                <li key={link}>
+                  <Link href={href}>{link}</Link>
+                </li>
+              );
+            })}
+          </ul>
+        </nav>
+      </header>
+      {newsContent}
+    </>
+  );
 }
