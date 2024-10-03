95951171fb5ddacd2fdf13aaf0ed6c3cb32c2638
hdbugsbunny <harshitpal26697@gmail.com>
2024-10-03 12:44:19 +0530

    Navigating Programmatically

--- news-blog/app/news/[newsId]/@modal/(.)image/page.js
+++ news-blog/app/news/[newsId]/@modal/(.)image/page.js
@@ -1,7 +1,10 @@
+"use client";
+
 import { DUMMY_NEWS } from "@/dummyNews";
-import { notFound } from "next/navigation";
+import { notFound, useRouter } from "next/navigation";
 
 export default function InterceptedImagePage({ params }) {
+  const router = useRouter();
   const { newsId } = params;
   const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.newsId === newsId);
   if (!newsItem) {
@@ -10,7 +13,7 @@ export default function InterceptedImagePage({ params }) {
 
   return (
     <>
-      <div className="modal-backdrop" />
+      <div className="modal-backdrop" onClick={router.back} />
       <dialog className="modal" open>
         <div className="fullscreen-image">
           <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
