aac7815c9ee333bad41e300b65e87cc652567575
hdbugsbunny <harshitpal26697@gmail.com>
2024-10-03 13:15:04 +0530

    Using Middleware

---
+++ news-blog/middleware.js
@@ -0,0 +1,11 @@
+import { NextResponse } from "next/server";
+
+export function middleware(request) {
+  console.log("🚀 ~ middleware ~ request:", request);
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: "/news",
+};
