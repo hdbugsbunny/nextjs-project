c662c4a91783c3ae554194ef52adfbfbdf7027ec
hdbugsbunny <harshitpal26697@gmail.com>
2024-10-04 15:01:42 +0530

    Validating User Input With The Help of The useFormState Hook & Adjusting Server Actions For useFormState

--- nextjs-blogs/app/new-blog/page.js
+++ nextjs-blogs/app/new-blog/page.js
@@ -1,38 +1,31 @@
-import FormSubmit from "@/components/form-submit";
+import BlogForm from "@/components/blog-form";
 import { storeBlog } from "@/lib/blogs";
 import { redirect } from "next/navigation";
 
 export default function NewBlogPage() {
-  async function createBlog(formData) {
+  async function createBlog(_, formData) {
     "use server";
     const title = formData.get("title");
     const image = formData.get("image");
     const content = formData.get("content");
+    let errors = [];
+    if (!title || title.trim().length === 0) {
+      errors.push("Title is required!");
+    }
+    if (!image || image.size === 0) {
+      errors.push("Image is required!");
+    }
+    if (!content || content.trim().length === 0) {
+      errors.push("Content is required!");
+    }
+
+    if (errors.length > 0) {
+      return { errors };
+    }
 
     await storeBlog({ imageUrl: "", title, content, userId: 1 });
     redirect("/feed");
   }
 
-  return (
-    <>
-      <h1>Create A New Blog</h1>
-      <form action={createBlog}>
-        <p className="form-control">
-          <label htmlFor="title">Title:</label>
-          <input type="text" id="title" name="title" />
-        </p>
-        <p className="form-control">
-          <label htmlFor="image">Image URL:</label>
-          <input type="file" accept="image/*" id="image" name="image" />
-        </p>
-        <p className="form-control">
-          <label htmlFor="content">Content:</label>
-          <textarea id="content" name="content" rows="5" />
-        </p>
-        <p className="form-actions">
-          <FormSubmit />
-        </p>
-      </form>
-    </>
-  );
+  return <BlogForm action={createBlog} />;
 }
