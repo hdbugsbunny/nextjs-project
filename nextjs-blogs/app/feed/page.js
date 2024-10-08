f5daf041d47650297a47c961c44c8f98ee4a59fa
hdbugsbunny <harshitpal26697@gmail.com>
2024-10-08 15:42:41 +0530

    Configuring Dynamic Page Metadata

--- nextjs-blogs/app/feed/page.js
+++ nextjs-blogs/app/feed/page.js
@@ -1,6 +1,16 @@
 import Blogs from "@/components/blogs";
 import { getBlogs } from "@/lib/blogs";
 
+export async function generateMetadata() {
+  const blogs = await getBlogs();
+  const numberOfBlogs = blogs.length;
+
+  return {
+    title: `NextJS ${numberOfBlogs} Blogs | Feed`,
+    description: "Discover Amazing Blogs From Our Community!",
+  };
+}
+
 export default async function FeedPage() {
   const blogs = await getBlogs();
 
