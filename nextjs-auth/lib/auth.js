e405981bb70364e3a5662739f184c5849e77f24d
hdbugsbunny <harshitpal26697@gmail.com>
2024-10-09 15:17:50 +0530

    Configuration a Session & a Session Cookie

--- nextjs-auth/lib/auth.js
+++ nextjs-auth/lib/auth.js
@@ -1,5 +1,6 @@
 import { BetterSqlite3Adapter } from "@lucia-auth/adapter-sqlite";
 import { Lucia } from "lucia";
+import { cookies } from "next/headers";
 import trainingDB from "./db";
 
 const adapter = new BetterSqlite3Adapter(trainingDB, {
@@ -13,3 +14,13 @@ const lucia = new Lucia(adapter, {
     attributes: { secure: process.env.NODE_ENV === "production" },
   },
 });
+
+export async function createAuthSession(userId) {
+  const session = await lucia.createSession(userId, {});
+  const sessionCookie = lucia.createSessionCookie(session.id);
+  cookies().set(
+    sessionCookie.name,
+    sessionCookie.value,
+    sessionCookie.attributes
+  );
+}
