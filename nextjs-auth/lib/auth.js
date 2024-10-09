2683ae56214caed1bbb2de4c92470cbce588635a
hdbugsbunny <harshitpal26697@gmail.com>
2024-10-09 15:11:50 +0530

    Creating a New Lucia Auth Instance

---
+++ nextjs-auth/lib/auth.js
@@ -0,0 +1,15 @@
+import { BetterSqlite3Adapter } from "@lucia-auth/adapter-sqlite";
+import { Lucia } from "lucia";
+import trainingDB from "./db";
+
+const adapter = new BetterSqlite3Adapter(trainingDB, {
+  user: "users",
+  session: "sessions",
+});
+
+const lucia = new Lucia(adapter, {
+  sessionCookie: {
+    expires: false,
+    attributes: { secure: process.env.NODE_ENV === "production" },
+  },
+});
