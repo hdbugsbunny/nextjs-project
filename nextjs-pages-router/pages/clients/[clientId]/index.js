451ae7ba9ba05a64e19d58f34352bea55aa765b1
hdbugsbunny <harshitpal26697@gmail.com>
2024-10-10 16:02:40 +0530

    Navigating Programmatically

--- nextjs-pages-router/pages/clients/[clientId]/index.js
+++ nextjs-pages-router/pages/clients/[clientId]/index.js
@@ -4,10 +4,18 @@ export default function ClientProjectsPage() {
   const router = useRouter();
   console.log("🚀 ~ ClientProjectsPage ~ router:", router);
 
+  const loadProjectHandler = () => {
+    router.push({
+      pathname: "/clients/[clientId]/[clientProjectId]",
+      query: { clientId: "harsh", clientProjectId: "project-a" },
+    });
+  };
+
   return (
     <div>
       <h1>Client Projects Page</h1>
       <p>This is the client projects page.</p>
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
