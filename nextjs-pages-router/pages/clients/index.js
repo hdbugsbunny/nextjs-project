bea5f5f6b59f665fa65cd7a6d31ce295bf1695f4
hdbugsbunny <harshitpal26697@gmail.com>
2024-10-10 15:18:32 +0530

    Navigating to Dynamic Routes

--- nextjs-pages-router/pages/clients/index.js
+++ nextjs-pages-router/pages/clients/index.js
@@ -1,8 +1,22 @@
+import Link from "next/link";
+
 export default function ClientsPage() {
+  const clients = [
+    { id: "harsh", name: "Harshit" },
+    { id: "jake", name: "Jake" },
+  ];
+
   return (
     <div>
       <h1>Clients Page</h1>
       <p>This is the clients page.</p>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
