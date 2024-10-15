91b3811a7bee044cd44d6567366943894606aabf
hdbugsbunny <harshitpal26697@gmail.com>
2024-10-15 14:59:38 +0530

    Extracting Data On The Catch-All Page

--- nextjs-events-pages-router/pages/events/[...slug].js
+++ nextjs-events-pages-router/pages/events/[...slug].js
@@ -1,8 +1,35 @@
+import EventList from "@/components/events/eventList";
+import { getFilteredEvents } from "@/dummyData";
+import { useRouter } from "next/router";
+
 export default function EventsFilteredPage() {
+  const router = useRouter();
+  const { slug } = router.query;
+  if (!slug) {
+    return <p className="center">Loading!!!</p>;
+  }
+
+  const numYear = +slug[0];
+  const numMonth = +slug[1];
+  if (
+    isNaN(numYear) ||
+    isNaN(numMonth) ||
+    numYear < 2023 ||
+    numYear > 2030 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return <p className="center">Invalid Date! Please Adjust Your Values!</p>;
+  }
+
+  const filteredEvents = getFilteredEvents({ year: numYear, month: numMonth });
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p className="center">No Events Found!</p>;
+  }
+
   return (
     <div>
-      <h1>Events Filtered Page</h1>
-      <p>This is the filtered events page.</p>
+      <EventList events={filteredEvents} />
     </div>
   );
 }
