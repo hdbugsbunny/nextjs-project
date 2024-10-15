ead09c28834953458e50055e1543ad0328b65093
hdbugsbunny <harshitpal26697@gmail.com>
2024-10-15 14:41:55 +0530

    Navigating To The "Filtered Events" Page Programmatically

--- nextjs-events-pages-router/components/events/eventsSearch.js
+++ nextjs-events-pages-router/components/events/eventsSearch.js
@@ -1,20 +1,34 @@
+import { useRef } from "react";
 import LinkButton from "../ui/linkButton";
 import classes from "./eventsSearch.module.css";
 
 export default function EventsSearch(props) {
+  const { onSearch } = props;
+  const yearRef = useRef();
+  const monthRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const selectedYear = yearRef.current.value;
+    const selectedMonth = monthRef.current.value;
+
+    onSearch(selectedYear, selectedMonth);
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.controls}>
         <div className={classes.control}>
           <label htmlFor="year">Year</label>
-          <select id="year">
+          <select id="year" ref={yearRef}>
             <option value="2023">2023</option>
             <option value="2024">2024</option>
           </select>
         </div>
         <div className={classes.control}>
           <label htmlFor="month">Month</label>
-          <select id="month">
+          <select id="month" ref={monthRef}>
             <option value="1">January</option>
             <option value="2">February</option>
             <option value="3">March</option>
--- nextjs-events-pages-router/pages/events/index.js
+++ nextjs-events-pages-router/pages/events/index.js
@@ -1,13 +1,19 @@
 import EventList from "@/components/events/eventList";
 import EventsSearch from "@/components/events/eventsSearch";
 import { getAllEvents } from "@/dummyData";
+import { useRouter } from "next/router";
 
 export default function EventsPage() {
+  const router = useRouter();
   const allEvents = getAllEvents();
+
+  const findEventsHandler = (year, month) => {
+    router.push(`/events/${year}/${month}`);
+  };
 
   return (
     <>
-      <EventsSearch />
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList events={allEvents} />
     </>
   );
