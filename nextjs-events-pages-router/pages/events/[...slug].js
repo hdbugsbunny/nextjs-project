54833815770fed285b9355259dfc3bc84713849b
hdbugsbunny <harshitpal26697@gmail.com>
2024-10-15 11:45:09 +0530

    Setting Up The Main Pages

---
+++ nextjs-events-pages-router/pages/events/[...slug].js
@@ -0,0 +1,8 @@
+export default function EventsFilteredPage() {
+  return (
+    <div>
+      <h1>Events Filtered Page</h1>
+      <p>This is the filtered events page.</p>
+    </div>
+  );
+}
---
+++ nextjs-events-pages-router/pages/events/[eventId]/index.js
@@ -0,0 +1,8 @@
+export default function EventDetailsPage() {
+  return (
+    <div>
+      <h1>Event Details Page</h1>
+      <p>This is the event details page.</p>
+    </div>
+  );
+}
