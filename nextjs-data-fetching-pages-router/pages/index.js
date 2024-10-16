661d9de8fe55456f2b5a55b16016572899607208
hdbugsbunny <harshitpal26697@gmail.com>
2024-10-16 12:20:22 +0530

    Utilizing Incremental Static Generation (ISR)

--- nextjs-data-fetching-pages-router/pages/index.js
+++ nextjs-data-fetching-pages-router/pages/index.js
@@ -14,9 +14,11 @@ export default function HomePage(props) {
 }
 
 export async function getStaticProps() {
+  console.log("Re-Generating...");
+
   const filePath = path.join(process.cwd(), "data", "dummyBackend.json");
   const jsonData = await fs.readFile(filePath);
   const data = JSON.parse(jsonData);
 
-  return { props: { products: data.products } };
+  return { props: { products: data.products }, revalidate: 10 };
 }
