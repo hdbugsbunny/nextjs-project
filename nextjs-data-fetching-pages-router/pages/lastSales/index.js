76f6ea861ee86d002bce099d85552a149cced31c
hdbugsbunny <harshitpal26697@gmail.com>
2024-10-16 17:19:20 +0530

    Combining Pre-Fetching With Client-Side Fetching

--- nextjs-data-fetching-pages-router/pages/lastSales/index.js
+++ nextjs-data-fetching-pages-router/pages/lastSales/index.js
@@ -1,8 +1,8 @@
 import { useEffect, useState } from "react";
 import useSWR from "swr";
 
-export default function LastSalesPage() {
-  const [sales, setSales] = useState();
+export default function LastSalesPage(props) {
+  const [sales, setSales] = useState(props.sales);
   //   const [isLoading, setIsLoading] = useState(false);
   const { data, error } = useSWR(
     "https://nextjs-temp-course-default-rtdb.firebaseio.com/sales.json",
@@ -37,7 +37,7 @@ export default function LastSalesPage() {
     return <p>Failed To Load!</p>;
   }
 
-  if (!data || !sales) {
+  if (!data && !sales) {
     return <p>Loading...</p>;
   }
 
@@ -53,4 +53,18 @@ export default function LastSalesPage() {
       </ul>
     </>
   );
+}
+
+export async function getStaticProps() {
+  const response = await fetch(
+    "https://nextjs-temp-course-default-rtdb.firebaseio.com/sales.json"
+  );
+  const data = await response.json();
+
+  const sales = [];
+  for (const key in data) {
+    sales.push({ id: key, ...data[key] });
+  }
+
+  return { props: { sales } };
 }
