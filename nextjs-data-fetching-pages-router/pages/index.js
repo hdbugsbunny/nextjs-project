0772a8af719634b05e01e53be1fca2b2661b9ecc
hdbugsbunny <harshitpal26697@gmail.com>
2024-10-16 11:54:37 +0530

    Adding "getStaticProps" To Pages

--- nextjs-data-fetching-pages-router/pages/index.js
+++ nextjs-data-fetching-pages-router/pages/index.js
@@ -1,9 +1,15 @@
-export default function HomePage() {
+export default function HomePage(props) {
+  const { products } = props;
+
   return (
     <ul>
-      <li>Product 1</li>
-      <li>Product 2</li>
-      <li>Product 3</li>
+      {products.map((product) => (
+        <li key={product.id}>{product.title}</li>
+      ))}
     </ul>
   );
+}
+
+export async function getStaticProps() {
+  return { props: { products: [{ id: "p1", title: "Product 1" }] } };
 }
