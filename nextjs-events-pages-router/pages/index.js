5768d16eec81323b648b2a620481fec7c94c5007
hdbugsbunny <harshitpal26697@gmail.com>
2024-10-17 11:24:35 +0530

    Adding Static Site Generation (SSG) On The Home Page

--- nextjs-events-pages-router/pages/index.js
+++ nextjs-events-pages-router/pages/index.js
@@ -1,12 +1,18 @@
 import EventList from "@/components/events/eventList";
-import { getFeaturedEvents } from "@/dummyData";
+import { getFeaturedEvents } from "@/helpers/utils";
 
-export default function HomePage() {
-  const featuredEvents = getFeaturedEvents();
+export default function HomePage(props) {
+  const { events } = props;
 
   return (
     <div>
-      <EventList events={featuredEvents} />
+      <EventList events={events} />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const events = await getFeaturedEvents();
+
+  return { props: { events }, revalidate: 10 };
 }
