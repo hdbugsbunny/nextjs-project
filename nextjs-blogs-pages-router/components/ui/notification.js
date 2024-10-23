87ebcbde6f82ac4569e8bf0a4a64d8d042025e9b
hdbugsbunny <harshitpal26697@gmail.com>
2024-10-23 16:24:52 +0530

    Using React Portals For Notifications Component

--- nextjs-blogs-pages-router/components/ui/notification.js
+++ nextjs-blogs-pages-router/components/ui/notification.js
@@ -1,3 +1,4 @@
+import ReactDOM from "react-dom";
 import classes from "./notification.module.css";
 
 export default function Notification({ title, message, status }) {
@@ -17,10 +18,11 @@ export default function Notification({ title, message, status }) {
 
   const cssClasses = `${classes.notification} ${statusClasses}`;
 
-  return (
+  return ReactDOM.createPortal(
     <div className={cssClasses}>
       <h2>{title}</h2>
       <p>{message}</p>
-    </div>
+    </div>,
+    document.getElementById("notifications")
   );
 }
