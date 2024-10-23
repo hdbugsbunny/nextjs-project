a921148b98c3ac4af3420ce64aef2bb20609046c
hdbugsbunny <harshitpal26697@gmail.com>
2024-10-23 14:17:40 +0530

    Adding The Contact API Route

---
+++ nextjs-blogs-pages-router/pages/api/contact.js
@@ -0,0 +1,22 @@
+export default function handler(req, res, next) {
+  if (req.method === "POST") {
+    const { name, email, message } = req.body;
+    if (
+      !name ||
+      name.trim() === "" ||
+      !email ||
+      !email.includes("@") ||
+      !message ||
+      message.trim() === ""
+    ) {
+      res.status(422).json({ message: "All Fields Are Required!" });
+      return;
+    }
+
+    // Send email to the server using a library like nodemailer
+    // ...
+    const newMessage = { name, email, message };
+
+    res.status(201).json({ message: "Message sent successfully!", newMessage });
+  }
+}
