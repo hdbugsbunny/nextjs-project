5f76d3403a47838fdb80fb720c0951fbaacf659d
hdbugsbunny <harshitpal26697@gmail.com>
2024-10-23 12:23:39 +0530

    Rendering Images With The "Next Image" Component (From Markdown)

--- nextjs-blogs-pages-router/components/posts/postDetails/postContent.js
+++ nextjs-blogs-pages-router/components/posts/postDetails/postContent.js
@@ -1,14 +1,38 @@
+import Image from "next/image";
 import ReactMarkdown from "react-markdown";
 import classes from "./postContent.module.css";
 import PostHeader from "./postHeader";
 
 export default function PostContent({ post }) {
   const formattedImage = `/images/posts/${post.image}`;
+  const customComponents = {
+    // img: ({ src, alt }) => (
+    //   <Image src={`/images/posts/${src}`} alt={alt} width={600} height={300} />
+    // ),
+    p: ({ children }) => {
+      if (children.type === "img") {
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${children.props.src}`}
+              alt={children.props.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+
+      return <p>{children}</p>;
+    },
+  };
 
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={formattedImage} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customComponents}>
+        {post.content}
+      </ReactMarkdown>
     </article>
   );
 }
