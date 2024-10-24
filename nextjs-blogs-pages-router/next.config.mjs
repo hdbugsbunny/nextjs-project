ac2e7a711b3b9a6a1ea0eaf8ab9e8048737c33dc
hdbugsbunny <harshitpal26697@gmail.com>
2024-10-24 12:30:12 +0530

    The NextJS Config File & Working With Environment Variables

--- nextjs-blogs-pages-router/next.config.mjs
+++ nextjs-blogs-pages-router/next.config.mjs
@@ -1,6 +1,22 @@
+const isProd = process.env.NODE_ENV === "production";
+const isDev = process.env.NODE_ENV === "development";
+const isTest = process.env.NODE_ENV === "test";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  ...(isDev && {
+    env: {
+      MONGODB_URI:
+        "mongodb+srv://harshit:QcN125pQlQGYmkrT@cluster0.kglxu6p.mongodb.net/my-blogs-dev?retryWrites=true&w=majority&appName=Cluster0",
+    },
+  }),
+  ...(isProd && {
+    env: {
+      MONGODB_URI:
+        "mongodb+srv://harshit:QcN125pQlQGYmkrT@cluster0.kglxu6p.mongodb.net/my-blogs?retryWrites=true&w=majority&appName=Cluster0",
+    },
+  }),
 };
 
 export default nextConfig;
