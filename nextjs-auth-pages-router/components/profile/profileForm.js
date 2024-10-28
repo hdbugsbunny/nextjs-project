190fdd9d7c4ff39ebbb37e10d9b013fe9e5f5021
hdbugsbunny <harshitpal26697@gmail.com>
2024-10-28 18:36:31 +0530

    Sending a "Change Password" Request From The Frontend

--- nextjs-auth-pages-router/components/profile/profileForm.js
+++ nextjs-auth-pages-router/components/profile/profileForm.js
@@ -1,15 +1,43 @@
+import { useRef } from "react";
 import classes from "./profileForm.module.css";
 
-export default function ProfileForm() {
+export default function ProfileForm({ changePassword }) {
+  const newPassRef = useRef();
+  const oldPassRef = useRef();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const newPassword = newPassRef.current.value;
+    const oldPassword = oldPassRef.current.value;
+
+    await changePassword(newPassword, oldPassword);
+
+    newPassRef.current.value = "";
+    oldPassRef.current.value = "";
+  };
+
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={handleSubmit}>
       <div className={classes.control}>
         <label htmlFor="newPassword">New Password</label>
-        <input name="newPassword" type="password" id="newPassword" required />
+        <input
+          ref={newPassRef}
+          name="newPassword"
+          type="password"
+          id="newPassword"
+          required
+        />
       </div>
       <div className={classes.control}>
         <label htmlFor="oldPassword">Old Password</label>
-        <input name="oldPassword" type="password" id="oldPassword" required />
+        <input
+          ref={oldPassRef}
+          name="oldPassword"
+          type="password"
+          id="oldPassword"
+          required
+        />
       </div>
       <div className={classes.action}>
         <button type="submit">Change Password</button>
--- nextjs-auth-pages-router/components/profile/userProfile.js
+++ nextjs-auth-pages-router/components/profile/userProfile.js
@@ -13,11 +13,26 @@ export default function UserProfile() {
   //   window.location.href = "/auth";
   //   return <p className={classes.profile}>Please Login!!!</p>;
   // }
+  const changePassword = async (newPassword, oldPassword) => {
+    const response = await fetch("/api/user/changePassword", {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ newPassword, oldPassword }),
+    });
+    const data = await response.json();
+    if (!response.ok) {
+      console.log("Error>>>", data.message);
+      return;
+    }
+    console.log(data.message);
+  };
 
   return (
     <section className={classes.profile}>
       <h1>Your User Profile</h1>
-      <ProfileForm />
+      <ProfileForm changePassword={changePassword} />
     </section>
   );
 }
