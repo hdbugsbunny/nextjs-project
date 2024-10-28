e3cd9137b7672a30e3a9c9e2eeb55bb7020119bc
hdbugsbunny <harshitpal26697@gmail.com>
2024-10-28 17:30:45 +0530

    Adding Server-Side Page Guards (And When to Use Which Approach)

--- nextjs-auth-pages-router/components/profile/userProfile.js
+++ nextjs-auth-pages-router/components/profile/userProfile.js
@@ -1,18 +1,18 @@
-import { useSession } from "next-auth/react";
+// import { useSession } from "next-auth/react";
 import ProfileForm from "./profileForm";
 import classes from "./userProfile.module.css";
 
 export default function UserProfile() {
-  const { data: session, status } = useSession();
-  const loading = status === "loading";
-  if (loading) {
-    return <p className={classes.profile}>Loading...</p>;
-  }
+  // const { data: session, status } = useSession();
+  // const loading = status === "loading";
+  // if (loading) {
+  //   return <p className={classes.profile}>Loading...</p>;
+  // }
 
-  if (!session) {
-    window.location.href = "/auth";
-    return <p className={classes.profile}>Please Login!!!</p>;
-  }
+  // if (!session) {
+  //   window.location.href = "/auth";
+  //   return <p className={classes.profile}>Please Login!!!</p>;
+  // }
 
   return (
     <section className={classes.profile}>
--- nextjs-auth-pages-router/pages/profile/index.js
+++ nextjs-auth-pages-router/pages/profile/index.js
@@ -1,5 +1,15 @@
 import UserProfile from "@/components/profile/userProfile";
+import { getSession } from "next-auth/react";
 
 export default function ProfilePage() {
   return <UserProfile />;
+}
+
+export async function getServerSideProps({ req, ...context }) {
+  const session = await getSession({ req });
+  if (!session) {
+    return { redirect: { destination: "/auth", permanent: false } };
+  }
+
+  return { props: { session } };
 }
