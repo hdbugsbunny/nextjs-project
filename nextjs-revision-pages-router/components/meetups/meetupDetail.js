0905bf93440f3eafe885f114358a3241ec558007
hdbugsbunny <harshitpal26697@gmail.com>
2024-11-05 19:09:10 +0530

    Adding Custom Components & Styling With CSS Modules

---
+++ nextjs-revision-pages-router/components/meetups/meetupDetail.js
@@ -0,0 +1,12 @@
+import styles from "./meetupDetail.module.css";
+
+export default function MeetupDetail({ image, title, address, description }) {
+  return (
+    <section className={styles.detail}>
+      <img src={image} alt={title} />
+      <h1>{title}</h1>
+      <address>{address}</address>
+      <p>{description}</p>
+    </section>
+  );
+}
--- nextjs-revision-pages-router/pages/[meetupId]/index.js
+++ nextjs-revision-pages-router/pages/[meetupId]/index.js
@@ -1,3 +1,14 @@
+import MeetupDetail from "@/components/meetups/meetupDetail";
+
 export default function MeetupDetailPage() {
-  return <h1>Meetup Detail Page</h1>;
+  const meetup = {
+    id: "m1",
+    title: "NextJS Meetup in Berlin",
+    image:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/Museumsinsel_Berlin_Juli_2021_1_%28cropped%29_b.jpg/1024px-Museumsinsel_Berlin_Juli_2021_1_%28cropped%29_b.jpg",
+    address: "Berlin, Germany",
+    description: "Join us for a free NextJS meetup!",
+  };
+
+  return <MeetupDetail {...meetup} />;
 }
