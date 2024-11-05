d0f5b4dcd46e29ed56f174979836e0325735477e
hdbugsbunny <harshitpal26697@gmail.com>
2024-11-05 17:24:23 +0530

    Starting Project Setup & Adding First Pages

---
+++ nextjs-revision-pages-router/pages/index.js
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Welcome to Next.js!</h1>
+      <p>This is your default home page.</p>
+    </div>
+  );
+}
---
+++ nextjs-revision-pages-router/pages/news/index.js
@@ -0,0 +1,8 @@
+export default function NewsPage() {
+  return (
+    <div>
+      <h1>News Page</h1>
+      <p>This is the news page.</p>
+    </div>
+  );
+}
