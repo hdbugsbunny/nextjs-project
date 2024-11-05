8c670ec5b6600ab627a7b816b06ceb67deb45fc1
hdbugsbunny <harshitpal26697@gmail.com>
2024-11-05 17:34:41 +0530

    Extracting Dynamic Route Data

--- nextjs-revision-pages-router/pages/news/[newsId].js
+++ nextjs-revision-pages-router/pages/news/[newsId].js
@@ -1,4 +1,10 @@
+import { useRouter } from "next/router";
+
 export default function NewsDetailPage() {
+  const router = useRouter();
+  const { newsId } = router.query;
+  if (!newsId) return <p>Loading...</p>;
+
   return (
     <div>
       <h1>News Detail Page</h1>
