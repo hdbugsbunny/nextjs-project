48af65a515180a65cd0875cf4059f38582973bc1
hdbugsbunny <harshitpal26697@gmail.com>
2024-11-06 12:50:42 +0530

    Working With Dynamic Path Params in getStaticProps & Dynamic Pages With getStaticPaths and getStaticProps

--- nextjs-revision-pages-router/pages/[meetupId]/index.js
+++ nextjs-revision-pages-router/pages/[meetupId]/index.js
@@ -1,8 +1,28 @@
 import MeetupDetail from "@/components/meetups/meetupDetail";
 
-export default function MeetupDetailPage() {
+export default function MeetupDetailPage({ meetup }) {
+  return <MeetupDetail {...meetup} />;
+}
+
+export async function getStaticPaths() {
+  // Fetch data from an API
+  const meetupIds = ["m1", "m2"];
+
+  // Generate paths for all meetup IDs
+  const paths = meetupIds.map((id) => ({
+    params: { meetupId: id },
+  }));
+
+  // Return paths
+  return { paths, fallback: false };
+}
+
+export async function getStaticProps(context) {
+  const { meetupId } = context.params;
+
+  // Fetch data from an API
   const meetup = {
-    id: "m1",
+    id: meetupId,
     title: "NextJS Meetup in Berlin",
     image:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/Museumsinsel_Berlin_Juli_2021_1_%28cropped%29_b.jpg/1024px-Museumsinsel_Berlin_Juli_2021_1_%28cropped%29_b.jpg",
@@ -10,5 +30,6 @@ export default function MeetupDetailPage() {
     description: "Join us for a free NextJS meetup!",
   };
 
-  return <MeetupDetail {...meetup} />;
+  // Pass data to the page via props
+  return { props: { meetup } };
 }
